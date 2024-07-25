1a25a6763ac5018c8b28ab189d81e5920dd12d9f
MAJJISAGAR <sagarmajji143@gmail.com>
2024-07-25 19:46:51 +0530

    added rama.sh

---
+++ rama.sh
@@ -0,0 +1,6 @@
+echo "This is GITHUB and Jenkins Integration"
+echo "Looks Cool!"
+echo "test is done"
+echo "Raj made change in github and code is pushed"
+echo "This is Github Jenkins Webhooks test"
+echo "We are testing"
