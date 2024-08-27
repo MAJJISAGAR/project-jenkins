279a2af56c584e61e8b3c54974f5ee47e3393de8
MAJJISAGAR <166007956+MAJJISAGAR@users.noreply.github.com>
2024-08-27 16:50:48 +0530

    Update rama.sh

--- rama.sh
+++ rama.sh
@@ -1,7 +1,7 @@
 echo "This is GITHUB and Jenkins Integration"
 echo "Looks Cool!"
 echo "test is done"
-echo "Sagar and pra made change in github and code is pushed"
+echo "Sagar and prasanth made change in github and code is pushed"
 echo "This is Github Jenkins Webhooks test"
 echo "We are testing"
 echo "This is sagar majji"
